05a465236aa469180b4e057b9ae29846de5d9269
RogueMoore <rogue.moore.private@gmail.com>
2024-01-05 15:38:19 +0200

    fixed media queries, now they work correctly on mobile devices. Footer container globally corrected. Added a burger-menu in the header for the mobile version

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,18 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    burger: document.querySelector('.burger-menu'),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.burger.addEventListener('click', toggleBurger);
 
   function toggleModal() {
     refs.modal.classList.toggle("backdrop--hidden");
+  }
+
+  function toggleBurger() {
+    refs.burger.classList.toggle('burger--open');
   }
 })();
