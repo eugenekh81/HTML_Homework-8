ff60b538568ff53c393531b7895d21c7212e2755
RogueMoore <rogue.moore.private@gmail.com>
2024-01-06 17:04:04 +0200

    Added burger-menu. Still a work in progress.

--- js/modal.js
+++ js/modal.js
@@ -3,18 +3,22 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
-    burger: document.querySelector('.burger-menu'),
+    burger: document.querySelector(".burger-menu"),
+    navMenu: document.querySelector("[data-nav-open]"),
   };
+
+  refs.navMenu.classList.add("nav-mobile");
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
-  refs.burger.addEventListener('click', toggleBurger);
+  refs.burger.addEventListener("click", toggleBurger);
 
   function toggleModal() {
     refs.modal.classList.toggle("backdrop--hidden");
   }
 
   function toggleBurger() {
-    refs.burger.classList.toggle('burger--open');
+    refs.burger.classList.toggle("burger--open");
+    refs.navMenu.classList.toggle("nav-mobile--open");
   }
 })();
